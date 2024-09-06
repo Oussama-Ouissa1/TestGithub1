7ff44d535738976afc53449cab16a26afb2f2aeb
Oussama-Ouissa1 <oussamaaouissa2006@gmail.com>
2024-09-06 19:41:42 +0100

    message

---
+++ test.js
@@ -0,0 +1,29 @@
+
+
+var x = 151 ;
+var y = 103 ;
+var z = x - y;
+console.log(z);
+
+
+
+var x, y, z;
+x = 151;
+y = 133;
+b = x - y ;
+w = x * y ;
+a = x / y ;
+console.log(b);
+console.log(w);
+console.log(a);
+
+
+
+
+function calcul(aficher) {
+    for ( let i=0; i>=10; i++){
+        let x = aficher * 9
+        console.log(i + " * 9 = " + x)
+    }
+}
+    calcul(9);
